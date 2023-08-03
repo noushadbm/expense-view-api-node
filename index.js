a0a75b4a60057bf78597c856afe641680722c921
noushadbm <noushadbm@gmail.com>
2023-08-03 23:19:37 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const db = require('./queries')
 const bodyParser = require('body-parser')
-const app = express()
-const port = 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
         extended: true,
